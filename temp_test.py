f2fbf4382351bd757bf9a919e0fd008d1d1e807c
ronyehezkel90 <ronyehezkel90@gmail.com>
2017-12-26 17:25:38 +0200

    add try except to call func

--- temp_test.py
+++ temp_test.py
@@ -1,7 +1,10 @@
 import sys
 
-script_name = sys.argv[1]
-if script_name == 'count_recs':
-    print '1'
-else:
-    print '2'
+try:
+    script_name = sys.argv[1]
+    if script_name == 'count_recs':
+        print '1'
+    else:
+        print '2'
+except:
+    print 'problem'
