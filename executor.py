c41e26e4ffb15e093ccbe9990eeaef312c18f1f2
ronyehezkel90 <ronyehezkel90@gmail.com>
2017-12-26 18:19:08 +0200

    machine debug

--- executor.py
+++ executor.py
@@ -3,10 +3,14 @@ from facebook_handler import FacebookHandler
 
 fb_handler = FacebookHandler()
 db_handler = DB_Handler()
-for i in range(1, 100):
-    posts_from_facebook = fb_handler.get_posts_from_facebook(i)
-    db_handler.insert_posts_to_mongo(posts_from_facebook)
-    print 'Posts from: {} days ago has been added'.format(str(i))
+for i in range(1, 20):
+    try:
+        posts_from_facebook = fb_handler.get_posts_from_facebook(i)
+        db_handler.insert_posts_to_mongo(posts_from_facebook)
+        print 'Posts from: {} days ago has been added'.format(str(i))
+    except:
+        print 'Posts from: {} days ago has been FAILED'.format(str(i))
+
 all_posts = db_handler.get_posts_from_mongo()
 db_handler.update_restaurants(all_posts)
 db_handler.print_restaurants_data()
